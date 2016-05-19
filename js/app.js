4c2cd439d7f7fe6dc1376ae1b17ab351682a01a7
Adam Smallcomb <atsmallcomb@gmail.com>
2016-05-19 16:03:56 -0400

    Add some build instructions and a second stepper motor option

--- js/app.js
+++ js/app.js
@@ -8,11 +8,11 @@ sidebarManager = new RegionManager({el: '#sidebar'});
 pages.home = {load: function(){
 	contentManager.show( new mainView());
 	$('#form').berry({actions:false,attributes:{interval: 15},fields:{
-			'interval': [3,4,5,6,7,8,9,10,15,100,200, 999]
+			'interval': {choices: [3,4,5,6,7,8,9,10,15,100,200, 999], type: 'custom_select'}
 		}
 	}).delay('change:interval', function(){
 		// debugger;
-		// console.log('i'+this.toJSON().interval+'\n');
+		console.log('i'+this.toJSON().interval+'\n');
 			chrome.serial.send(connectionId, convertStringToArrayBuffer('i'+this.toJSON().interval+'\n'), function(){})
 
 	})
